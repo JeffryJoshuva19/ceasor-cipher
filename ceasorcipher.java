072799c9bcbdec58fb911924e31dd22a6c32248f
JeffryJoshuva19 <111883780+JeffryJoshuva19@users.noreply.github.com>
2023-08-18 11:49:01 +0530

    Update ceasorcipher.java

--- ceasorcipher.java
+++ ceasorcipher.java
@@ -1,5 +1,5 @@
 import java.util.*;
-//code for ceasorcipher
+//code for ceasorcipher 
 class ceasor{  
     public static void main(String args[]){  
         Scanner sc=new Scanner(System.in);
@@ -56,3 +56,4 @@ class ceasor{
         }            
     }  
 } 
+//by jeffry joshuva amalan j
